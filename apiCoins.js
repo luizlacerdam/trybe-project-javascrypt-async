307fc5eb77dbce405d3da878939ab7d8501b3e27
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-09-11 16:05:45 -0300

    requisito-2

--- apiCoins.js
+++ apiCoins.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 
 const fetchCoins = async () => {
   const url = 'https://api.coincap.io/v2/assets';
@@ -9,10 +9,23 @@ const fetchCoins = async () => {
   // } catch (error) {
   //   console.log('Algo deu errado: ', error);
   // }
-  const request = await fetch(url);
-  request.then((response) => response.json())
-  .then((data) => console.log(data.data))
-  .catch((error) => console.log('Error! Algo deu errado', error));
-};
 
-fetchCoins();
+  const request = fetch(url);
+  return request.then((response) => response.json())
+  .then((data) => data.data)
+  .catch((error) => console.log(`Error! Algo deu errado: ${error}`));
+};
+// return array
+
+function createHTML(nome, simbolo, preco) {
+  const ul = document.getElementById('coins-list');
+  const element = document.createElement('li');
+  element.innerText = `${nome} (${simbolo}): ${preco}`;
+  ul.appendChild(element);
+}
+
+function makeData() {
+  fetchCoins().then((data) => data.forEach((element) => 
+    createHTML(element.name, element.symbol, element.priceUsd)));
+}
+makeData();
