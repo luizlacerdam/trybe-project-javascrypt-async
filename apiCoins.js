9769459ee4c031e4205632ae2453b6b3208a675f
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-09-11 10:13:42 -0300

    requisito-1

--- apiCoins.js
+++ apiCoins.js
@@ -1,4 +1,18 @@
+const fetch = require('node-fetch');
+
 const fetchCoins = async () => {
+  const url = 'https://api.coincap.io/v2/assets';
+  // try { 
+  //   const request = await fetch(url);
+  //   const data = await request.json();
+  //   console.log(data.data);
+  // } catch (error) {
+  //   console.log('Algo deu errado: ', error);
+  // }
+  const request = await fetch(url);
+  request.then((response) => response.json())
+  .then((data) => console.log(data.data))
+  .catch((error) => console.log('Error! Algo deu errado', error));
 };
 
 fetchCoins();
